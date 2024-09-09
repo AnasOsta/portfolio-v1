cce421a8fd420db7c3dfd0b262fa73cdbdb632aa
AnasOsta <onichan418@gmail.com>
2024-09-09 14:12:49 +0300

    ...

--- src/app/components/Email.jsx
+++ src/app/components/Email.jsx
@@ -58,7 +58,7 @@ export default function Email() {
     <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4">
       <button onClick={test}>test</button>
       <div>
-        <h5 className="text-xl font-bold text-white my-2">Let`s Connect</h5>
+        <h5 className="text-xl font-bold text-white my-2">Let s Connect</h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
           {" "}
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -10,7 +10,7 @@ export default function Hero() {
       <div className="grid grid-cols-1 lg:grid-cols-12">
         <div className="col-span-7 place-self-center text-center md:text-left">
           <h1 className="text-white mb-4 sm:text-5xl lg:text-6xl text-4xl font-extrabold">
-            <span>Hello, I`m </span>
+            <span>Hello, Im </span>
             <br></br>
             <TypeAnimation
               sequence={[
